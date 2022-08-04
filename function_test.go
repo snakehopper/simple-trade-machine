d496b2de3158118576e8612c5f7ab712c2b8f4a5
snakehopper <snakehopper@gmail.com>
2022-08-05 02:25:55 +0800

    parse new signal format

---
+++ function_test.go
@@ -0,0 +1,25 @@
+package function
+
+import (
+	"ghohoo.solutions/yt/internal/data"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewAlert(t *testing.T) {
+	dat := []struct {
+		input  string
+		output data.Alert
+	}{
+		{"左側拐點｜多方訊號｜1小時｜$0.00001188", data.LONG},
+		{"順勢指標｜空轉多訊號｜30分鐘｜$23322.13", data.LONG},
+		{"順勢指標｜多方平倉訊號｜30分鐘｜$23322.13", data.CLOSE},
+		{"左側拐點｜多方平倉訊號｜1小時｜$0.000012", data.CLOSE},
+		{"順勢指標｜多方減倉訊號｜30分鐘｜$23371.23", data.REDUCE},
+		{"左側拐點｜多方停損訊號｜1小時｜$0.0000117802", data.STOP_LOSS},
+	}
+
+	for _, d := range dat {
+		assert.Equal(t, d.output, data.NewAlert(d.input))
+	}
+}
--- internal/data/types.go
+++ internal/data/types.go
@@ -1,6 +1,9 @@
 package data
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 const (
 	UnknownAlert Alert = iota
@@ -12,22 +15,49 @@ const (
 
 type Alert int
 
+type AlertMessage struct {
+	Strategy string
+	Signal   string
+	Kline    string
+	Price    string
+}
+
+func NewAlertMessage(s string) (*AlertMessage, error) {
+	s2 := strings.Split(s, "｜")
+	if len(s2) != 4 {
+		return nil, fmt.Errorf("unknown string format: %s", s)
+	}
+
+	return &AlertMessage{
+		Strategy: s2[0],
+		Signal:   s2[1],
+		Kline:    s2[2],
+		Price:    s2[3],
+	}, nil
+}
+
 func NewAlert(s string) Alert {
 	if len(s) == 0 {
 		return UnknownAlert
 	}
 
-	switch a := []rune(s)[0]; a {
-	case '多':
+	msg, err := NewAlertMessage(s)
+	if err != nil {
+		fmt.Println(msg)
+		return UnknownAlert
+	}
+
+	switch msg.Signal {
+	case "空轉多訊號", "多方訊號":
 		return LONG
-	case '減':
+	case "多方減倉訊號":
 		return REDUCE
-	case '平':
+	case "多方平倉訊號":
 		return CLOSE
-	case '停':
+	case "多方停損訊號":
 		return STOP_LOSS
 	default:
-		fmt.Printf("unknown alert:%v len:%d\n", a, len(s))
+		fmt.Printf("unknown alert:%v len:%d\n", s, len(s))
 		return UnknownAlert
 	}
 }
