8adca8da571ce3f1e2168a9bfa4f16b8cb530bb8
snakehopper <snakehopper@gmail.com>
2022-08-20 13:51:06 +0800

    ftx: fix get market min notional error

--- ftx/structs/markets.go
+++ ftx/structs/markets.go
@@ -36,28 +36,28 @@ type MarketResponse struct {
 type Market struct {
 	Name                  string  `json:"name"`
 	Enabled               bool    `json:"enabled"`
-	PostOnly              bool    `json:"post_only"`
-	PriceIncrement        float64 `json:"price_increment"`
-	SizeIncrement         float64 `json:"size_increment"`
-	MinProvideSize        float64 `json:"min_provide_size"`
+	PostOnly              bool    `json:"postOnly"`
+	PriceIncrement        float64 `json:"priceIncrement"`
+	SizeIncrement         float64 `json:"sizeIncrement"`
+	MinProvideSize        float64 `json:"minProvideSize"`
 	Last                  float64 `json:"last"`
 	Bid                   float64 `json:"bid"`
 	Ask                   float64 `json:"ask"`
 	Price                 float64 `json:"price"`
 	Type                  string  `json:"type"`
-	FutureType            string  `json:"future_type,omitempty"`
-	BaseCurrency          string  `json:"base_currency,omitempty"`
-	IsEtfMarket           bool    `json:"is_etf_market"`
-	QuoteCurrency         string  `json:"quote_currency"`
-	Underlying            string  `json:"underlying,omitempty"`
+	FutureType            *string `json:"futureType,omitempty"`
+	BaseCurrency          *string `json:"baseCurrency,omitempty"`
+	IsEtfMarket           bool    `json:"isEtfMarket"`
+	QuoteCurrency         *string `json:"quoteCurrency,omitempty"`
+	Underlying            *string `json:"underlying,omitempty"`
 	Restricted            bool    `json:"restricted"`
-	HighLeverageFeeExempt bool    `json:"high_leverage_fee_exempt"`
-	LargeOrderThreshold   float64 `json:"large_order_threshold"`
-	Change1h              float64 `json:"change_1_h"`
-	Change24h             float64 `json:"change_24_h"`
-	ChangeBod             float64 `json:"change_bod"`
-	QuoteVolume24h        float64 `json:"quote_volume_24_h"`
-	VolumeUsd24h          float64 `json:"volume_usd_24_h"`
-	PriceHigh24h          float64 `json:"price_high_24_h"`
-	PriceLow24h           float64 `json:"price_low_24_h"`
+	HighLeverageFeeExempt bool    `json:"highLeverageFeeExempt"`
+	LargeOrderThreshold   float64 `json:"largeOrderThreshold"`
+	Change1H              float64 `json:"change1h"`
+	Change24H             float64 `json:"change24h"`
+	ChangeBod             float64 `json:"changeBod"`
+	QuoteVolume24H        float64 `json:"quoteVolume24h"`
+	VolumeUsd24H          float64 `json:"volumeUsd24h"`
+	PriceHigh24H          float64 `json:"priceHigh24h"`
+	PriceLow24H           float64 `json:"priceLow24h"`
 }
